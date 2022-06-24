1edb2a5b52cf38e5f9b35675961695ed1905fd8f
ChetanGavanali <chetangavanali@gmail.com>
2022-06-24 23:28:07 +0530

    Added TotalWageByCompany

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,37 +1,64 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    public static final int IS_PART_TIME =1;
+    public static final int IS_FULL_TIME =2;
 
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
 
-    public static int computeEmployeeWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth) {
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
 
-        int empHrs = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
-        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+
+
+    public EmployeeWage (String company, int empRatePerHour,
+                         int numOfWorkingDays, int maxHoursPerMonth) {
+
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
+
+
+    public void computeEmpWage () {
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays=0;
+
+        while (totalEmpHrs <= maxHoursPerMonth &&
+                totalWorkingDays < numOfWorkingDays) {
             totalWorkingDays++;
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            int empCheck = (int) Math.floor(Math.random () * 10)% 3;
             switch (empCheck) {
                 case IS_PART_TIME:
                     empHrs = 4;
+                    break;
+
                 case IS_FULL_TIME:
                     empHrs = 8;
                     break;
+
                 default:
                     empHrs = 0;
             }
-            totalEmpHrs =+ empHrs;
-            System.out.println("Day: " + totalWorkingDays + " EmpHr: " + empHrs);
+
+            totalEmpHrs += empHrs;
+            System.out.println ("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
         }
-        int totalEmpWage = totalEmpHrs * empRatePerHr;
-        System.out.println("Total Emp Wage: " + totalEmpWage);
-        return totalEmpWage;
+        totalEmpWage = totalEmpHrs * empRatePerHour;
+    }
+    @Override
+    public String toString () {
+        return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage;
     }
 
-    public static void main(String[] args) {
-        computeEmployeeWage("Dell", 20,2,10);
-        computeEmployeeWage("Asus",10,4,20);
+    public static void main (String[] args) {
+        EmployeeWage Dell = new EmployeeWage ("Dell", 20, 2, 10);
+        EmployeeWage Asus = new EmployeeWage ("Asus", 20, 2, 10);
+        Dell.computeEmpWage ();
+        System .out .println (Dell);
+        Asus.computeEmpWage ();
+        System .out .println (Asus) ;
     }
 }
